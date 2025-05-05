159cffa201b0d926bbb28eb2e2f8ea8cac06041a
Codified11 <ltalreja177@gmail.com>
2025-05-05 20:57:13 +0530

    Add some files

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/File_Hidder?useSSL=false","root", "database_lata_talreja");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+//        System.out.println("Connection ho gaya saab");
+        return connection;
+    }
+
+    public static void closeConnection(){
+        if(connection != null){
+            try{
+                connection.close();
+            }
+            catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
