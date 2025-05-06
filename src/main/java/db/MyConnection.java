a808a3b6968a2b2f50338a2271b7d20af3a928cc
Codified11 <ltalreja177@gmail.com>
2025-05-07 00:35:27 +0530

    Add some files

--- src/main/java/db/MyConnection.java
+++ src/main/java/db/MyConnection.java
@@ -9,7 +9,7 @@ public class MyConnection {
     public static Connection getConnection(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/File_Hidder?useSSL=false","root", "database_lata_talreja");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/File_Hidder?useSSL=false","root", "/* enter your database password */");
 
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
